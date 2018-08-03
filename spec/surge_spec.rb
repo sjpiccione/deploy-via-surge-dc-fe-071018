4bfb38fc024cceb380797a9bdd5270414b835b3f
Steven Piccione <sjpiccione@gmail.com>
2018-08-03 00:54:22 +0000

    automatically backed up by learn

--- spec/surge_spec.rb
+++ spec/surge_spec.rb
@@ -2,14 +2,14 @@ require_relative './spec_helper'
 
 SOURCE_FILE="http://two-bite.surge.sh/"
 
-describe "The file #{SOURCE_FILE}" do
+describe "The file #{http://two-bite.surge.sh/}" do
   it 'should be present' do
-    file_is_present = File.exists?(SOURCE_FILE)
-    expect(file_is_present).to be_truthy, "Make sure you created a file called #{SOURCE_FILE} with your URL in it."
+    file_is_present = File.exists?(http://two-bite.surge.sh/)
+    expect(file_is_present).to be_truthy, "Make sure you created a file called #{http://two-bite.surge.sh/} with your URL in it."
   end
 
   it 'should contain a url to a surge.sh URL like http://dusty-rose.surge.sh' do
-    url = File.open(SOURCE_FILE).readline
+    url = File.open(http://two-bite.surge.sh/).readline
     expect(url.match(/^http.*\.surge\.sh\z/)).to be_truthy
   end
 end
